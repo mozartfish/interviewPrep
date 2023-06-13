fc86e1acfe38b5c4b0045645413925a455ca563a
Pranav Rajan <rajan.pranav320@gmail.com>
2023-06-12 22:52:43 -0700

    Day 6 - Trees

---
+++ Day 6 - Trees /binaryTreeRightSideView.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        from collections import deque 
+        q = deque() 
+        result = []
+        if root:
+            q.append(root)
+        while len(q) > 0:
+            rightNode = None
+            # do something 
+            for i in range(len(q)):
+                curr = q.popleft() 
+                rightNode = curr 
+                if curr.left:
+                    q.append(curr.left)
+                if curr.right:
+                    q.append(curr.right)
+            if rightNode:
+                result.append(rightNode.val)
+        return result
+            
+        
