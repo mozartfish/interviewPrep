2018880579655e314ee591b4d95fa2977d45de43
Pranav Rajan <rajan.pranav320@gmail.com>
2023-06-23 09:36:13 -0700

    Day 7 - Backtracking

---
+++ Day 7 - Backtracking /combinationSum.py
@@ -0,0 +1,21 @@
+# runtime - 0(2^n)
+
+lass Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        subset = []
+        result = []
+        totalSum = 0
+
+        def dfs(i, subset, totalSum):
+            if totalSum == target:
+                result.append(subset.copy())
+                return 
+            if i > len(candidates) - 1 or totalSum > target:
+                return 
+            subset.append(candidates[i])
+            dfs(i, subset, totalSum + candidates[i])
+            subset.pop() 
+            dfs(i + 1, subset, totalSum)
+        
+        dfs(0, subset, totalSum)
+        return result
