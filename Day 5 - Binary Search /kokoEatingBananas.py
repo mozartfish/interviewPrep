90d3306f4951faf2806056ec4631e4edfcfef014
Pranav Rajan <rajan.pranav320@gmail.com>
2023-09-30 19:17:36 -0700

    Finish Day 5 - Binary Searcha

---
+++ Day 5 - Binary Search /kokoEatingBananas.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        lo = 1 
+        hi = max(piles)
+        result = hi 
+
+        while lo <= hi:
+            k = (lo + hi) // 2 
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile / k)
+            if hours <= h:
+                result = min(result, k)
+                hi = k - 1 
+            else:
+                lo = k + 1 
+        
+        return result
