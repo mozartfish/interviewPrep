c2216a7258d35cb1a3d682943f1d77761ade4e15
Pranav Rajan <rajan.pranav320@gmail.com>
2023-10-09 18:43:10 -0700

    Remove old files

--- oldSolutions/fibonacci.py
+++
@@ -1,8 +0,0 @@
-# runtime - O(2^n)
-# space - O(n) additional space because we are using additional stack space for every recursive call 
-class Solution:
-    def fib(self, n: int) -> int:
-        if n <= 1:
-            return n 
-        return self.fib(n - 1) + self.fib(n - 2)
-        
