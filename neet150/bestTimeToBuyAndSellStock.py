db26221dd1fa3a15ccc716a75de1ecf7b7dd2606
Pranav Rajan <rajan.pranav320@gmail.com>
2023-11-05 14:14:24 -0800

    clean up stuff

--- neet150/bestTimeToBuyAndSellStock.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        maxProfit = 0
-        minPrice = prices[0]
-        for p in prices:
-            if p < minPrice:
-                minPrice = p 
-            maxProfit = max(maxProfit, p - minPrice)
-        return maxProfit
