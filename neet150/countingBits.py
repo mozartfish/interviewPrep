b38bdeaf6124e9ab2138d484b23655d1eceb4da9
Pranav Rajan <rajan.pranav320@gmail.com>
2023-11-20 09:01:07 -0800

    Restart the neet 150 list

--- neet150/countingBits.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        result = [0] * (n + 1)
-        offset = 1 
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i 
-            result[i] = 1 + result[i - offset]
-        return result
--- neet150/missingNumber.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def missingNumber(self, nums: List[int]) -> int:
-        result = len(nums)
-        n = len(nums)
-
-        for i in range(n):
-            result += i - nums[i]
-        
-        return result
