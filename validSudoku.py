2aa67c2bce13de9125a329ae781717b03dc60381
Pranav Rajan <rajan.pranav320@gmail.com>
2023-12-07 14:36:41 -0800

    Fix validSudoku code formatting

--- validSudoku.py
+++ validSudoku.py
@@ -5,22 +5,19 @@ class Solution:
       Ideas
       - use sets to keep track of unique values
       '''
-        from collections import defaultdict
-        ROWS = defaultdict(set)
-        COLS = defaultdict(set)
-        box = defaultdict(set) # key = (r/3, c/3)
-        
-        for r in range(9):
-          for c in range(9):
-            if board[r][c] == ".":
-              continue
-            if (board[r][c] in ROWS[r] or 
-            board[r][c] in COLS[c] or 
-            board[r][c] in box[(r//3, c//3)]):
-              return False 
-          
-            ROWS[r].add(board[r][c])
-            COLS[c].add(board[r][c])
-            box[(r//3, c//3)].add(board[r][c])
-        
-        return True 
+      from collections import defaultdict
+      ROWS = defaultdict(set)
+      COLS = defaultdict(set)
+      box = defaultdict(set) # key = (r/3, c/3)
+      for r in range(9):
+        for c in range(9):
+          if board[r][c] == ".":
+            continue
+          if (board[r][c] in ROWS[r] or
+          board[r][c] in COLS[c] or
+          board[r][c] in box[(r//3, c//3)]):
+            return False 
+          ROWS[r].add(board[r][c])
+          COLS[c].add(board[r][c])
+          box[(r//3, c//3)].add(board[r][c])
+      return True 
