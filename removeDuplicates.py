28c67aefe2a3938b0fd3a06f015df06012636497
Pranav Rajan <rajan.pranav320@gmail.com>
2024-03-14 11:13:36 -0700

    Day 1 - Static + Dynamic Arrays

---
+++ removeDuplicates.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        k = 1 
+        i = 1 
+        while i < len(nums):
+            if nums[i] == nums[i - 1]:
+                i += 1 
+            else:
+                nums[k] = nums[i]
+                i += 1 
+                k += 1 
+        return k 
+        
